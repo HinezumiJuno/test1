11a82ecd8d26425447dfd91610c1029e00b3c24d
Hinezumi <8Juno.Minazuki8@gmail.com>
2016-12-25 17:04:48 +0900

    初期登録を実施。

---
+++ vagrant/site-cookbooks/phpenv/metadata.rb
@@ -0,0 +1,8 @@
+name             'phpenv'
+maintainer       'Flaming Mouse Project'
+maintainer_email '8Juno.Minazuki8@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures phpenv'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'hostsfile'
